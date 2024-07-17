4c706b7c57b3635e07e8dfe7b677aa1934f09870
Surya Ghai <suryaghai@techforgoodinc.org>
2024-07-17 13:04:15 -0400

    Update PageNotFound.tsx

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 export const PageNotFound=()=>{
     return(
-        <div>
-            Page not found
+        <div className="container">
+                <div className="column1">
+                    <h1>
+                        404: Page Not Found
+                    </h1>
+                    <p>
+                        The requested URL doesn't exist on our servers.
+                    </p>
+                    <Link to="/">
+                        <button type="button" className="btn">
+                            Click here to go back to the main page
+                        </button>
+                    </Link>
+                </div>
+                <div className="column2">
+                    <img src="https://i.ytimg.com/vi/Z_Wpkd-wrxQ/hqdefault.jpg"></img>
+                </div>          
         </div>
     )
 
